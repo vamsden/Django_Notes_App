337402b853f0d892bb3fd6d6bfc95612b4c1488e
vanstinger <vandykfernandes@hotmail.com>
2016-05-05 17:01:59 +0530

    Created empty documentation using sphinx

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Django App Store&#8217;s documentation!"],titleterms:{app:0,django:0,document:0,indic:0,store:0,tabl:0,welcom:0}})
